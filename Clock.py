daf8670e15a12236d4ca10849b07845d5103ee75
Abasiama Okonna <calixtookonna@gmail.com>
2022-07-24 05:21:45 +0100

    This is a project showing python codes for making a digital clock, the code editor used here  is pycharm.

---
+++ Clock.py
@@ -0,0 +1,12 @@
+my_list = [1,4,6,8,4.3,6.5]
+my_list.sort()
+print("my new list looks this:", my_list)
+my_list[3] = "Hello student"
+print("my revamped list looks like this:", my_list)
+my_list = my_list[1:4]
+print("my update list looks like this", my_list)
+
+
+
+
+
